21515a2203393bd1407a5f782db667d880eb8673
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2025-04-10 15:31:11 +0200

    add plot_settings

---
+++ meta/plot_settings.py
@@ -0,0 +1,14 @@
+# # plot/LaTeX settings
+# plt.rcParams.update({
+#     "text.usetex": True,
+#     "font.family": "Bitstream Vera Sans",
+#     'font.size': 11.0,
+#     'text.latex.preamble': r"\usepackage{upgreek}",
+#     'mathtext.fontset': 'custom',
+#     'mathtext.rm': 'Bitstream Vera Serif',
+#     'mathtext.it': 'Bitstream Vera Serif:italic',
+#     'mathtext.bf': 'Bitstream Vera Serif:bold'
+# })
+
+
+colors = ["tab:pink", "tab:purple", "tab:blue", "tab:cyan", "tab:green", "tab:olive", "tab:red", "tab:orange"]
