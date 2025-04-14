10ee53e4fdb9bcfd9bcb04ed480de8c6188401bc
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2025-04-14 15:57:35 +0200

    add frequency sweep for plot of IC calculation results

--- dev/script_infinite_cylinder_vs_fem.py
+++ dev/script_infinite_cylinder_vs_fem.py
@@ -3,54 +3,54 @@ from utils import infinite_cylinder as ic
 from utils.physics import *
 from matplotlib import pyplot as plt
 
-
-# Problem definition
-R = 7.5e-3
-T_c = 65
-f = 550e3
-A = 23.4
-
 # load material data
 df_mu = materials.read_permeability_txt2df(material_name="N49")
 df_eps = materials.read_permittivity_txt2df(material_name="N49")
 # print(df_mu)
 # print(df_eps)
 
-# Interpolate / Extrapolate material data for f/T-operation point
-eps = materials.eps_from_df(df_eps, f, T_c)
-# print(eps)
-
-mu_h = materials.mu_h_from_df(df_mu, f, T_c)
-
-
-# exemplary magnetic (test) field
-h_eg = np.linspace(0, 50, 20)
-# print(mu_h(h_eg))
-
 fig, ax = plt.subplots(6, sharex=True)
-# Calculate the magnetic field for the infinite cylinder
-r_, H_, E_ = ic.r_h_e_(R=R, f=f, A=A, eps=eps, mu_h=mu_h)
-ax[0].plot(r_, H_)
 
-# Plot permeability distribution
-ax[1].plot(r_, mu_h(H_).real/mu_0)
-ax[2].plot(r_, mu_h(H_).imag/mu_0)
+fs = [500e3, 600e3, 700e3, 800e3, 900e3, 1000e3]
+for f in fs:
+    # Problem definition
+    R = 7.5e-3
+    T_c = 50
+    phi_max = 13e-6
 
-# Calculate magnetic flux density from material law
-B_ = mu_h(H_) * H_
-ax[3].plot(r_, B_*1000)
+    # Interpolate / Extrapolate material data for f/T-operation point
+    eps = materials.eps_from_df(df_eps, f, T_c)
+    mu_h = materials.mu_h_from_df(df_mu, f, T_c)
 
-# flux
-flux = ic.flux_from_b_(r_, B_)
-print(f"magnetic_flux = {np.round(abs(flux)*1e6, 3)} µVs (from analytical IC model)")
+    # exemplary magnetic (test) field
+    h_eg = np.linspace(0, 50, 100)
+    # print(mu_h(h_eg))
 
-# Plot the electric field
-ax[4].plot(r_, E_)
+    A = 0
+    flux = 0
+    while not abs(flux) > phi_max:
+        # Calculate the magnetic field for the infinite cylinder
+        r_, H_, E_ = ic.r_h_e_(R=R, f=f, A=A, eps=eps, mu_h=mu_h)
 
-# Calcultate the dielectric flux from the material law
-D_ = eps * E_
-ax[5].plot(r_, D_)
+        # Calculate magnetic flux density from material law
+        B_ = mu_h(H_) * H_
 
+        # flux
+        flux = ic.flux_from_b_(r_, B_)
+        print(f"magnetic_flux = {np.round(abs(flux)*1e6, 3)} µVs (from analytical IC model)")
+
+        # Calcultate the dielectric flux from the material law
+        D_ = eps * E_
+
+        A = A + 0.1
+
+    # plot all fields
+    ax[0].plot(r_, H_, label=f"{int(f/1000)} kHz")
+    ax[1].plot(r_, mu_h(H_).real/mu_0)
+    ax[2].plot(r_, mu_h(H_).imag/mu_0)
+    ax[3].plot(r_, B_*1000)
+    ax[4].plot(r_, E_)
+    ax[5].plot(r_, D_)
 
 ax[0].set_ylabel(r"$H$ in A/m")
 ax[1].set_ylabel(r"$\mu_\mathrm{real}$")
@@ -58,6 +58,8 @@ ax[2].set_ylabel(r"$\mu_\mathrm{imag}$")
 ax[3].set_ylabel(r"$B$ in mT")
 ax[4].set_ylabel(r"$E$ in V/m")
 ax[5].set_ylabel(r"$D$ in As/m²")
+
+ax[0].legend()
 
 ax[0].grid()
 ax[1].grid()
