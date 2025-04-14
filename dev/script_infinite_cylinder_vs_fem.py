2ff29af63d5782207bd7944569f8e217f280aa0e
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2025-04-14 14:04:01 +0200

    add plotting of all field results according to the IC model

--- dev/script_infinite_cylinder_vs_fem.py
+++ dev/script_infinite_cylinder_vs_fem.py
@@ -22,32 +22,48 @@ eps = materials.eps_from_df(df_eps, f, T_c)
 
 mu_h = materials.mu_h_from_df(df_mu, f, T_c)
 
+
+# exemplary magnetic (test) field
 h_eg = np.linspace(0, 50, 20)
 # print(mu_h(h_eg))
 
-
+fig, ax = plt.subplots(6, sharex=True)
 # Calculate the magnetic field for the infinite cylinder
 r_, H_, E_ = ic.r_h_e_(R=R, f=f, A=A, eps=eps, mu_h=mu_h)
-# plt.plot(r_, H_)
+ax[0].plot(r_, H_)
 
 # Plot permeability distribution
-# plt.plot(r_, mu_h(H_).real/mu_0)
-# plt.plot(r_, mu_h(H_).imag/mu_0)
+ax[1].plot(r_, mu_h(H_).real/mu_0)
+ax[2].plot(r_, mu_h(H_).imag/mu_0)
 
 # Calculate magnetic flux density from material law
 B_ = mu_h(H_) * H_
-plt.plot(r_, B_)
+ax[3].plot(r_, B_*1000)
 
 # flux
 flux = ic.flux_from_b_(r_, B_)
 print(f"magnetic_flux = {np.round(abs(flux)*1e6, 3)} µVs (from analytical IC model)")
 
 # Plot the electric field
-# plt.plot(r_, E_)
+ax[4].plot(r_, E_)
 
 # Calcultate the dielectric flux from the material law
 D_ = eps * E_
-# plt.plot(r_, D_)
+ax[5].plot(r_, D_)
 
-plt.grid()
+
+ax[0].set_ylabel(r"$H$ in A/m")
+ax[1].set_ylabel(r"$\mu_\mathrm{real}$")
+ax[2].set_ylabel(r"$\mu_\mathrm{imag}$")
+ax[3].set_ylabel(r"$B$ in mT")
+ax[4].set_ylabel(r"$E$ in V/m")
+ax[5].set_ylabel(r"$D$ in As/m²")
+
+ax[0].grid()
+ax[1].grid()
+ax[2].grid()
+ax[3].grid()
+ax[4].grid()
+ax[5].grid()
+
 plt.show()
