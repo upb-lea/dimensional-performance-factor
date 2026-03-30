c0649a9da6d764014338839396e60a001916c77e
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2026-03-30 15:51:08 +0200

    removed unused non linear formulationd of IC model

--- utils/infinite_cylinder.py
+++ utils/infinite_cylinder.py
@@ -1,13 +1,8 @@
-import numpy as np
-from scipy.integrate import trapezoid
 from scipy import special
-from scipy.integrate import solve_bvp
-from scipy.interpolate import interp1d
-from utils.physics import *
 from utils.maths import *
 
 
-def r_h_e_linear(R, f, A, eps: complex, mu: complex, n_runs=5, print_wavelength=False):
+def r_h_e_linear(R, f, A, eps: complex, mu: complex, print_wavelength=False):
     """
 
     :param R:
@@ -30,77 +25,4 @@ def r_h_e_linear(R, f, A, eps: complex, mu: complex, n_runs=5, print_wavelength=
         print(f"wavelength at {f} Hz: {wavelength*1000} mm")
         print(f"diameter-to-wavelength ratio at {f} Hz: {2*R/wavelength}")
     return r_, H_, E_
-
-
-def r_h_e_(R, f, A, eps: complex, mu_of_h, n_runs=5):
-    """
-
-    :param R:
-    :param f:
-    :param A:
-    :param eps: complex permittivity (not the relative permittivity)
-    :param mu_of_h: complex permeability (not the relative permeability)
-    :param n_runs:
-    :return:
-    """
-    permittivity = eps
-    r_ = np.linspace(0, R, 50)
-    H_LF = np.ones_like(r_) * A
-    H_ = H_LF
-    E_ = np.zeros_like(H_)
-    runs = np.arange(0, n_runs)
-    for run in runs:
-        permeability = mu_of_h(np.abs(H_))
-        k_ = 2 * np.pi * f * np.sqrt(permeability * permittivity)
-        H_ = A * special.jv(0, k_ * r_) / special.jv(0, k_ * R)
-        E_ = A * k_ / (j * 2 * np.pi * f * permittivity) * special.jv(1, k_ * r_) / special.jv(0, k_ * R)
-    return r_, H_, E_
-
-
-def r_h_e_NL(R, f, A, eps: complex, mu_h, n_runs=10):
-    """
-
-    :param mu_h:
-    :param R:
-    :param f:
-    :param A:
-    :param eps: complex permittivity (not the relative permittivity)
-    :param mu: complex permeability (not the relative permeability)
-    :param n_runs:
-    :return:
-    """
-    r_ = np.linspace(1e-5, R, 50)
-
-    permittivity = eps
-    H_vals_interpolation = np.linspace(0, 70, 200)  # TODO: this should be an argument with regard to the material
-    permeability = mu_h(H_vals_interpolation)
-    k_at_h = 2 * np.pi * f * np.sqrt(permeability * permittivity)
-
-    k_squared_interp = interp1d(np.abs(H_vals_interpolation), k_at_h**2, kind="linear", fill_value='extrapolate')
-
-    # Define the ODE system
-    def bessel_system(r, y):
-        H, dH = y
-        k2 = k_squared_interp(np.abs(H))
-        d2H = -(1 / r) * dH - (k2 - 1) * H
-        d2H[r == 0] = - (k2[r == 0] - 1) * H[r == 0]  # handle r=0
-        return np.vstack((dH, d2H))
-
-    def bc(ya, yb):
-        return np.array([
-            yb[0] - A,  # H(R) = 0.01 (for example)
-            ya[1]  # dH/dr at r=0 = 0 (symmetry)
-        ])
-
-    H_guess = np.exp(-r_)  # or any smooth function that meets BC roughly
-    guess = np.vstack((H_guess, -H_guess))
-
-    # Solve BVP
-    sol = solve_bvp(bessel_system, bc, r_, guess)
-
-    H_ = sol.y[0]
-    dH_dr = sol.y[1]
-    E_ = (1 / (1j * 2 * np.pi * f * eps)) * dH_dr
-    return sol.x, H_, E_
-
 
