768950be81cc2eba2fc4955cbc1e4774638b1a7a
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2026-03-31 14:18:42 +0200

    update paths

--- meta/paths.py
+++ meta/paths.py
@@ -5,8 +5,7 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'folders.json'), 'r') as file:
     folder_links = json.load(file)
 
-comsol_results = folder_links["comsol_results"]
-IPEC_results = folder_links["IPEC_results"]
 material_data = folder_links["material_data"]
+simulation_data = folder_links["simulation_data"]
+measurement_data = folder_links["measurement_data"]
 grafics = folder_links["grafics"]
-measurements = folder_links["measurements"]
